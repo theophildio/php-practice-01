1e356e3f4ba687d6326eb36981c1fbbb9b575c36
Theophil Dio <dtheo.sangma@gmail.com>
2023-09-13 15:59:52 +0600

    HTML added to repo to check the weather report

---
+++ weather-report/weather_report.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather Report</title>
+    <style>
+        <?php include "style.css";?>
+    </style>
+</head>
+<body>
+    <div class="contaienr">
+        <h2 class="title">Weather Report</h2>
+        <form class="input-form" action="" method="POST">
+            <input type="number" name="number">
+            <div class="buttons">
+                <input type="submit" name="check_weather" value="Check the weather">
+            </div>
+        </form>
+        <div class="result">
+            <span>
+
+            </span>
+        </div>
+    </div>
+</body>
+</html>
