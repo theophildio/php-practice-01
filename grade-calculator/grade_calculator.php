9af4271102cd1ccc2b7b6a6663df81a2237770d4
Theophil Dio <dtheo.sangma@gmail.com>
2023-09-13 23:21:21 +0600

    Error fixed

--- grade-calculator/grade_calculator.php
+++ grade-calculator/grade_calculator.php
@@ -1,32 +1,43 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $number1 = $_POST["number1"];
-    $number2 = $_POST["number2"];
-    $number3 = $_POST["number3"];
-    $calculate = isset($_POST["average"]);
 
-    if ($calculate) {
-        $totalNumber = $number1 + $number2 + $number3;
-        $calculateAverage = $totalNumber / 3;
-        $average = ceil($calculateAverage);
-    }
-    if ($number1 <= 32 || $number2 <= 32 || $number3 <= 32) {
-        $grade = "<span class=\"fail\">One subject fail</span>";
-    } else {
-        if ($average >= 80 && $average <= 100) {
-            $grade = "A+";
-        } elseif ($average >= 70 && $average <= 79) {
-            $grade = "A";
-        } elseif ($average >= 60 && $average <= 69) {
-            $grade = "A-";
-        } elseif ($average >= 50 && $average <= 59) {
-            $grade = "B";
-        } elseif ($average >= 40 && $average <= 99) {
-            $grade = "C";
-        } elseif ($average >= 33 && $average <= 39) {
-            $grade = "D";
-        } else {
-            $grade = "F";
+global $average;
+global $grade;
+global $warning;
+
+$number1 = isset($_POST["number1"]) ? $_POST["number1"] : null;
+$number2 = isset($_POST["number2"]) ? $_POST["number2"] : null;
+$number3 = isset($_POST["number3"]) ? $_POST["number3"] : null;
+
+if (isset($_POST["number1"]) && isset($_POST["number2"]) && isset($_POST["number3"])) {
+    if ($number1 == "" || $number2 == "" || $number3 == "") {
+        $warning = "Please enter valid number!";
+    } elseif ($number1 < 0 || $number2 < 0 || $number3 < 0) {
+        $warning = "Please enter valid number!";
+    } elseif (isset($_POST["number1"]) && isset($_POST["number2"]) && isset($_POST["number3"])) {
+        if ($number1 <= 32 || $number2 <= 32 || $number3 <= 32) {
+            $grade = "<span class=\"fail\">Sorry! Result is Fail</span>";
+            $totalNumber = $number1 + $number2 + $number3;
+            $calculateAverage = $totalNumber / 3;
+            $average = ceil($calculateAverage);
+        } elseif ($number1 >= 32 || $number2 >= 32 || $number3 >= 32) {
+            $totalNumber = $number1 + $number2 + $number3;
+            $calculateAverage = $totalNumber / 3;
+            $average = ceil($calculateAverage);
+            if ($average >= 80 && $average <= 100) {
+                $grade = "A+";
+            } elseif ($average >= 70 && $average <= 79) {
+                $grade = "A";
+            } elseif ($average >= 60 && $average <= 69) {
+                $grade = "A-";
+            } elseif ($average >= 50 && $average <= 59) {
+                $grade = "B";
+            } elseif ($average >= 40 && $average <= 99) {
+                $grade = "C";
+            } elseif ($average >= 33 && $average <= 39) {
+                $grade = "D";
+            } else {
+                $grade = "F";
+            }
         }
     }
 }
@@ -50,8 +61,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <input type="number" name="number1" placeholder="Input number">
             <input type="number" name="number2" placeholder="Input number">
             <input type="number" name="number3" placeholder="Input number">
+            <span>
+                <?php echo $warning; ?>
+            </span>
             <div class="buttons">
-                <input type="submit" name="average" value="Get result">
+                <input type="submit" name="calculate" value="Get result">
             </div>
         </form>
         <div class="result">
