ae143395b196051328d424be534b951a2bd12825
Theophil Dio <dtheo.sangma@gmail.com>
2023-09-13 11:01:20 +0600

    Task 1: Personal Information Page added to repo

--- personal_info.php
+++ personal_info.php
@@ -1,1 +1,18 @@
+<?php
 
+// Task 1: Personal Information Page
+
+$name = "Theophil Dio";
+$age = 34;
+$country = "Bangladesh";
+$aboutMe = "I'm a web developer focused on crafting clean and
+user‑friendly experiences, passionate about building excellent
+software that improves the lives of those around me";
+
+echo "My name is $name.";
+echo PHP_EOL;
+echo "I am $age years old.";
+echo PHP_EOL;
+echo "I live in $country.";
+echo PHP_EOL;
+echo "A brief introduction of me: $aboutMe.";
